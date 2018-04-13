4f7c3c7d8777dce21087f3b9043028df64983ea5
iraha <e17001@std.it-college.ac.jp>
2018-04-13 20:24:02 +0900

    preparing the layout
    talkingの帰ってきた結果を、吹き出しで表示するようにCSSを編集。

--- talking.php
+++ talking.php
@@ -15,10 +15,10 @@ if(isset($_SESSION['repeat'])){
   $repeat = '';
 }
 
-if(isset($_SESSION['messege'])){
-  $tanuMesseges = $_SESSION['messege'];
-  var_dump($tanuMesseges);
-}
+// if(isset($_SESSION['messege'])){
+  // $tanuMesseges = $_SESSION['messege'];
+  // var_dump($tanuMesseges);
+// }
 
 // $ensei = $talk->getAll();
 // var_dump($ensei);
@@ -28,6 +28,7 @@ if(isset($_SESSION['messege'])){
 <html>
   <head>
     <title>人口無脳たぬき</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
     <link rel='stylesheet' href='<?php echo h($css); ?>' />
     <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script> -->
     
@@ -38,22 +39,30 @@ if(isset($_SESSION['messege'])){
   <body>
     <header><h1>おしゃべり</h1></header>
     <main>
-    <p id="token"><?php echo '何か言って！'; ?></p>
-      <input id="input_word" type="text" name="input_word">
-      <input type="button" id="btn" value='さにわ' class="btn bt-start">
-    <form action="" id="saniwa_form" method="post">
-      <input type="hidden" id="mode" name="mode" value="">
-      <input type="hidden" id="saniwa_message" name="saniwa_message" value="">
-    </form>
-
-    <p><div id="repeat">
-      <?php echo $repeat; ?>
-    </div></p>
-    <div id="tanuki">
-      <?php foreach ($tanuMesseges as $key => $ms) : ?>
-        <p><?php echo $ms; ?></p>
-      <?php endforeach; ?>
-    </div>
+    <content>
+      <p id="token"><?php echo '何か言って！'; ?></p>
+        <input id="input_word" type="text" name="input_word">
+        <input type="button" id="btn" value='発言' class="btn bt-start">
+      <form action="" id="saniwa_form" method="post">
+        <input type="hidden" id="mode" name="mode" value="">
+        <input type="hidden" id="saniwa_message" name="saniwa_message" value="">
+      </form>
+    </content>
+    <content id="saniwa">
+      <div class="balloon-left"><?php echo $repeat; ?></div>
+    </content>
+    <content id="tanuki">
+      <?php if(isset($_SESSION['messege'])): ?>
+        <?php $tanuMesseges = $_SESSION['messege']; ?>
+        <?php foreach ($tanuMesseges as $key => $ms) : ?>
+          <div style="display: block">
+          <div class="balloon-right">
+              <?php echo $ms; ?>
+          </div>
+          </div>
+        <?php endforeach; ?>
+      <?php endif; ?>
+    </content>
     </main>
   </body>
 </html>
